a613e08ac9ca978c4ee3ec21ce90db0307865f45
Saadat366 <sadanaza@gmail.com>
2020-07-09 05:03:39 +0600

    +static and media

--- product/views.py
+++ product/views.py
@@ -1,3 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
-# Create your views here.
+def products(request):
+    return HttpResponse("<h1>список товаров</h1>")
---
+++ readme.txt
@@ -0,0 +1,3 @@
+Добавьте settings.py + туда:
+1) добавьте core, product, feedback в INSTALLED_APPS
+2) добавьте STATIC_URL, STATIC_ROOT, MEDIA_URL, MEDIA_ROOT
